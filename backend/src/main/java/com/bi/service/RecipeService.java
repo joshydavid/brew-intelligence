ff20127a85a63c10bd9a26280bfda02d51138908
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-05-06 18:00:22 +0800

    feat: add post recipe API (#4)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/bi/service/RecipeService.java
+++ backend/src/main/java/com/bi/service/RecipeService.java
@@ -4,10 +4,13 @@ package com.bi.service;
 import java.util.List;
 import java.util.UUID;
 
+import com.bi.dto.AddRecipeDTO;
 import com.bi.dto.RecipeDTO;
 
 public interface RecipeService {
     List<RecipeDTO> getRecipes();
+
+    RecipeDTO addRecipe(AddRecipeDTO dto);
 
     RecipeDTO updateRecipe(UUID id, RecipeDTO updatedRecipe);
 }
