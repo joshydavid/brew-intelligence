57f3470dcb25f8e4686bc8d463f7c8f81f79e301
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-05-06 20:04:00 +0800

    feat: validate api request body
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

---
+++ backend/src/main/java/com/bi/constant/ErrorMessage.java
@@ -0,0 +1,21 @@
+package com.bi.constant;
+
+public class ErrorMessage {
+    // AddCoffeeListingDTO
+    public static final String COFFEE_NAME_REQUIRED = "Coffee name is required";
+    public static final String ROAST_DATE_REQUIRED = "Roast date is required";
+    public static final String NO_FUTURE_DATE = "Date cannot be in the future";
+    public static final String WEIGHT_REQUIRED = "Weight is required";
+    public static final String POSITIVE_ONLY = "Number should never be zero or negative";
+    public static final String ROAST_TYPE_REQUIRED = "Roast type is required";
+    public static final String BREW_METHOD_REQUIRED = "Brew method is required";
+
+    // AddRecipeDTO
+    public static final String METHOD_TYPE_REQUIRED = "Method type is required";
+    public static final String COFFEE_DOSE_REQUIRED = "Coffee dose is required";
+    public static final String WATER_AMOUNT_REQUIRED = "Water amount is required";
+    public static final String BREW_TEMP_REQUIRED = "Brew temperature is required";
+    public static final String BREW_TIME_REQUIRED = "Brew time is required";
+    public static final String BREW_INSTRUCTIONS_REQUIRED = "Brew instructions are required";
+    public static final String CREATED_BY_REQUIRED = "Creator's name is required";
+}
