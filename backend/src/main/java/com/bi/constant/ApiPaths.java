bd84e64778fb3a91f67ee505d23d68135c79a958
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-05-06 13:07:12 +0800

    fix: split service layer into interface and implementation (#2)
    * feat: add redis cache
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * fix: update redis config
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * feat: recipe API
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * fix: rename variable to be more descriptive
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * fix: refactor receipe service layer into interface
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    ---------
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/bi/constant/ApiPaths.java
+++ backend/src/main/java/com/bi/constant/ApiPaths.java
@@ -2,6 +2,6 @@ package com.bi.constant;
 
 public class ApiPaths {
     private static final String BASE_API = "/api/v1";
-    public static final String GET_COFFEE_LISTING = BASE_API + "/coffee-listings";
+    public static final String GET_COFFEE_LISTINGS = BASE_API + "/coffee-listings";
     public static final String GET_RECIPES = BASE_API + "/recipes";
 }
