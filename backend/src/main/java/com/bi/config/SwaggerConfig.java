fbcf8d66a214e7a3f8dc42e7736af2274f027079
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-05-07 17:12:26 +0800

    fix: remove hardcoded swagger api url
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/bi/config/SwaggerConfig.java
+++ backend/src/main/java/com/bi/config/SwaggerConfig.java
@@ -2,20 +2,23 @@ package com.bi.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.bi.constant.ApiPaths;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${springdoc.swagger-ui.url}")
+    private String swaggerApiUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         Server server = new Server();
-        server.setUrl(ApiPaths.SWAGGER_API_URL);
+        server.setUrl(swaggerApiUrl);
         server.setDescription("Production Server");
         return new OpenAPI().servers(List.of(server));
     }
