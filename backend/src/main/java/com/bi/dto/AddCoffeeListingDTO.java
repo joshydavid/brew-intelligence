b268a729043897adc9d82139d4625e0bced5f9e3
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-05-07 13:29:12 +0800

    feat: add dtos
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

---
+++ backend/src/main/java/com/bi/dto/AddCoffeeListingDTO.java
@@ -0,0 +1,33 @@
+package com.bi.dto;
+
+import java.util.Date;
+
+import com.bi.constant.ErrorMessage;
+import com.bi.model.enums.BrewMethod;
+import com.bi.model.enums.RoastType;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+public class AddCoffeeListingDTO {
+    @NotBlank(message = ErrorMessage.COFFEE_NAME_REQUIRED)
+    private String coffeeName;
+
+    @NotNull(message = ErrorMessage.ROAST_DATE_REQUIRED)
+    @PastOrPresent(message = ErrorMessage.NO_FUTURE_DATE)
+    private Date roastDate;
+
+    @NotNull(message = ErrorMessage.WEIGHT_REQUIRED)
+    @Positive(message = ErrorMessage.POSITIVE_ONLY)
+    private Integer weightInKg;
+
+    @NotNull(message = ErrorMessage.ROAST_TYPE_REQUIRED)
+    private RoastType roastType;
+
+    @NotNull(message = ErrorMessage.BREW_METHOD_REQUIRED)
+    private BrewMethod brewMethod;
+}
---
+++ backend/src/main/java/com/bi/dto/AddOrUpdateRecipeDTO.java
@@ -0,0 +1,34 @@
+package com.bi.dto;
+
+import java.util.List;
+
+import com.bi.constant.ErrorMessage;
+import com.bi.model.enums.MethodType;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class AddOrUpdateRecipeDTO {
+    @NotNull(message = ErrorMessage.METHOD_TYPE_REQUIRED)
+    private MethodType methodType;
+
+    @NotNull(message = ErrorMessage.COFFEE_DOSE_REQUIRED)
+    private Double coffeeDose;
+
+    @NotNull(message = ErrorMessage.WATER_AMOUNT_REQUIRED)
+    private Double waterAmount;
+
+    @NotNull(message = ErrorMessage.BREW_TEMP_REQUIRED)
+    private Integer brewTemp;
+
+    @NotBlank(message = ErrorMessage.BREW_TIME_REQUIRED)
+    private String brewTime;
+
+    @NotNull(message = ErrorMessage.BREW_INSTRUCTIONS_REQUIRED)
+    private List<String> brewInstructions;
+
+    @NotBlank(message = ErrorMessage.CREATED_BY_REQUIRED)
+    private String createdBy;
+}
