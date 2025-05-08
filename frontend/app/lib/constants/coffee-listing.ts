b4e15d17b1cabb31b799248f1e3176aa8bd7d3e1
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-05-08 20:39:25 +0800

    feat: init nextjs
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

---
+++ frontend/app/lib/constants/coffee-listing.ts
@@ -0,0 +1,12 @@
+export enum BrewMethod {
+  ESPRESSO = "Espresso",
+  V60 = "V60",
+  CHEMEX = "Chemex",
+  MOKA_POT = "Moka Pot",
+}
+
+export enum RoastType {
+  LIGHT = "Light",
+  MEDIUM = "Medium",
+  DARK = "Dark",
+}
