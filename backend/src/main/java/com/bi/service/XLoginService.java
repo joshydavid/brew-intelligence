af09eaaf21a602b9bdfe2c2bc669a2cee6b78425
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-05-10 17:16:53 +0800

    fix: split logic into service layer for x controller
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/bi/service/XLoginService.java
+++ backend/src/main/java/com/bi/service/XLoginService.java
@@ -7,6 +7,9 @@ import com.github.scribejava.core.model.OAuth1AccessToken;
 import com.github.scribejava.core.model.OAuth1RequestToken;
 import com.github.scribejava.core.oauth.OAuth10aService;
 
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 public interface XLoginService {
     OAuth10aService getService();
 
@@ -16,4 +19,12 @@ public interface XLoginService {
 
     OAuth1AccessToken getAccessToken(OAuth1RequestToken requestToken, String oAuthVerifier)
             throws IOException, ExecutionException, InterruptedException;
+
+    public void handleCallback(OAuth1RequestToken requestToken,
+            String oAuthVerifier,
+            HttpSession session,
+            HttpServletResponse response, OAuth1AccessToken accessToken) throws Exception;
+
+    public String getUser(OAuth10aService service, OAuth1AccessToken accessToken)
+            throws IOException, InterruptedException, ExecutionException;
 }
--- backend/src/main/java/com/bi/service/impl/XLoginServiceImpl.java
+++ backend/src/main/java/com/bi/service/impl/XLoginServiceImpl.java
@@ -6,15 +6,29 @@ import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.bi.constant.Authentication;
 import com.bi.service.XLoginService;
 import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.model.OAuth1AccessToken;
 import com.github.scribejava.core.model.OAuth1RequestToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth10aService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Service
 public class XLoginServiceImpl implements XLoginService {
+    @Value("${frontend.redirect.url}")
+    private String frontendRedirectUrl;
+
+    @Value("${cookie.secure}")
+    private Boolean isCookieSecure;
+
     private final OAuth10aService service;
 
     public XLoginServiceImpl(
@@ -49,5 +63,34 @@ public class XLoginServiceImpl implements XLoginService {
             String oAuthVerifier)
             throws IOException, ExecutionException, InterruptedException {
         return this.service.getAccessToken(requestToken, oAuthVerifier);
+    }
+
+    @Override
+    public void handleCallback(OAuth1RequestToken requestToken,
+            String oAuthVerifier,
+            HttpSession session,
+            HttpServletResponse response, OAuth1AccessToken accessToken) throws Exception {
+
+        Cookie cookie = new Cookie(Authentication.X_ACCESS_TOKEN, accessToken.getToken());
+        cookie.setHttpOnly(true);
+        cookie.setSecure(isCookieSecure);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 60);
+        response.addCookie(cookie);
+
+        String userProfile = this.getUser(service, accessToken);
+        session.setAttribute(Authentication.USER, userProfile);
+
+        response.sendRedirect(frontendRedirectUrl);
+    }
+
+    @Override
+    public String getUser(OAuth10aService service, OAuth1AccessToken accessToken)
+            throws IOException, InterruptedException, ExecutionException {
+        OAuthRequest request = new OAuthRequest(Verb.GET,
+                "https://api.twitter.com/1.1/account/verify_credentials.json");
+        service.signRequest(accessToken, request);
+        Response response = service.execute(request);
+        return response.getBody();
     }
 }
