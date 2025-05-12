c7fa6e3b16a0cdd50cd352b2cd114997b1bcc7fe
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-05-13 06:07:19 +0800

    fix: ui enhancements
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- frontend/app/chat/ChatHelper.tsx
+++ frontend/app/chat/ChatHelper.tsx
@@ -1,6 +1,6 @@
 export default function ChatHelper() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center gap-8">
+    <div className="flex flex-1 flex-col items-center justify-center gap-8 px-2">
       <h1 className="text-4xl font-semibold text-gray-900 dark:text-white">
         👋 Hey, I'm your{" "}
         <span className="text-blue-600 dark:text-blue-500">AI Barista.</span>
