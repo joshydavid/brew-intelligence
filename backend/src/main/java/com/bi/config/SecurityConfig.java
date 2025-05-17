8e3b895b743ee4fd15a4105be3fcf21af11348a9
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-05-17 13:23:54 +0800

    chore: deployment set up (#49)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/bi/config/SecurityConfig.java
+++ backend/src/main/java/com/bi/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.bi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,8 +13,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(ApiPaths.HEALTHCHECK).permitAll()
                         .requestMatchers(ApiPaths.LOGIN_WITH_X).permitAll()
                         .requestMatchers(ApiPaths.LOGIN_WITH_X + ApiPaths.X_CALLBACK).permitAll()
                         .anyRequest()
@@ -23,5 +22,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
