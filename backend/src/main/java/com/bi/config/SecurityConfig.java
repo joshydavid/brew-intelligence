d51ed40edff754c1308c19f5d3d2364b47dd52a1
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-05-17 13:53:58 +0800

    fix broken route
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/bi/config/SecurityConfig.java
+++ backend/src/main/java/com/bi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,6 +14,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(ApiPaths.HEALTHCHECK).permitAll()
                         .requestMatchers(ApiPaths.LOGIN_WITH_X).permitAll()
