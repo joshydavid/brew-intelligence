8263b6731ca7dd24ca61ae53aef7d55cf211f0ba
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-05-21 19:47:21 +0800

    feat: llm chat ui (#52)
    * feat: llm chat ui
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * fix: schema typo error
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    ---------
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

---
+++ frontend/app/chat/index.tsx
@@ -0,0 +1,3 @@
+export { default as ChatHelperText } from "./ChatHelperText";
+export { default as ChatInput } from "./ChatInput";
+export { default as ChatLLM } from "./ChatLLM";
--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -1,29 +1,40 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
-import { APP_AI } from "@/lib/constants/marketing";
-import { cn } from "@/lib/constants/utils";
+import { Form } from "@/components/ui/form";
+import { aiChatSchema, AIChatSchema } from "@/schema/ai-chat";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import ChatHelper from "./ChatHelper";
-import ChatLLM from "./ChatLLM";
+import { useForm } from "react-hook-form";
+import { ChatHelperText, ChatInput, ChatLLM } from "./index";
 
 export default function Chat() {
   const [showHelper, setShowHelper] = useState<boolean>(true);
+  const chatForm = useForm<AIChatSchema>({
+    resolver: zodResolver(aiChatSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  const { control, handleSubmit, reset } = chatForm;
+
+  const onSubmit = (data: AIChatSchema) => {
+    // TDOO: llm api
+    console.log("Sent:", data.message);
+    reset();
+  };
 
   return (
     <div className="mx-auto flex min-h-[calc(100vh-100px)] w-full max-w-2xl flex-col p-8">
-      {showHelper ? <ChatHelper /> : <ChatLLM />}
-      <footer className="rounded-lg">
-        <Input
-          className={cn(
-            "md:text-md text-md rounded-full text-gray-200 shadow-none focus:ring-0 focus-visible:ring-0 md:text-sm dark:text-gray-400",
-            "mx-auto max-w-2xl animate-border border border-transparent",
-            "[background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box]",
-            "py-7 pl-7",
-          )}
-          placeholder={APP_AI.INPUT_PLACEHOLDER}
-        />
-      </footer>
+      {showHelper ? <ChatHelperText /> : <ChatLLM />}
+      <Form {...chatForm}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mt-4 flex items-center gap-2"
+        >
+          <ChatInput control={control} />
+        </form>
+      </Form>
     </div>
   );
 }
