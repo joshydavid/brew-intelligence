3c0da682cd523a5597377aaade34829ef716c092
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-06-07 11:08:23 +0800

    fix: component color in light mode (#55)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- frontend/app/chat/ChatInput.tsx
+++ frontend/app/chat/ChatInput.tsx
@@ -15,9 +15,10 @@ export default function ChatInput({ control }: { control: any }) {
           <Input
             {...field}
             className={cn(
-              "md:text-md rounded-full text-sm text-gray-200 shadow-none focus:ring-0 focus-visible:ring-0 md:text-sm dark:text-gray-400",
+              "md:text-md rounded-full text-sm text-gray-600 shadow-none focus:ring-0 focus-visible:ring-0 md:text-sm dark:text-gray-400",
               "mx-auto max-w-2xl animate-border border border-transparent",
-              "[background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box]",
+              "dark:[background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box]",
+              "bg-gray-100",
               "py-7 pr-18 pl-7",
             )}
             placeholder={APP_AI.INPUT_PLACEHOLDER}
