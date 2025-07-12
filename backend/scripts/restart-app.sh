d59ecdfbe1745e2f26ed470f69668de852afa950
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-07-12 15:45:57 +0800

    fix: script clean up (#64)
    * chore: allow api docs to be publicly accessible
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * chore: script clean up
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    ---------
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/scripts/restart-app.sh
+++ backend/scripts/restart-app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source /home/ec2-user/myapp/.env.prod
 
-pkill -f 'java -jar /home/ec2-user/myapp/backend/target/brewintelligence-backend-0.0.1-SNAPSHOT.jar'
-nohup java -jar /home/ec2-user/myapp/backend/target/brewintelligence-backend-0.0.1-SNAPSHOT.jar > /home/ec2-user/myapp/backend/target/brewintelligence-backend-0.0.1-SNAPSHOT.log 2>&1 &
-echo "App restarted"
+JAR_PATH="/home/ec2-user/myapp/backend/target/brewintelligence-backend-0.0.1-SNAPSHOT.jar"
+JAR_DIR="$(dirname "$JAR_PATH")"
+LOG_FILE="$JAR_DIR/brewintelligence-backend-0.0.1-SNAPSHOT.log"
+
+pkill -f "java -jar $JAR_PATH"
+nohup java -jar "$JAR_PATH" > "$LOG_FILE" 2>&1 &
+echo "App restarted. Logs: $LOG_FILE"
