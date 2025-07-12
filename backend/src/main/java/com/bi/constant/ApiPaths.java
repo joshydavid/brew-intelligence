85eebe884591b26102a51843715b99876a17e0c4
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-07-12 22:39:46 +0800

    fix: prefix healthcheck endpoint with versioning (#66)
    * fix: script file path
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * fix: prefix healthcheck endpoint with versioning
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * fix: remove healthcheck endpoint temporarily
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>
    
    * Revert "fix: remove healthcheck endpoint temporarily"
    
    This reverts commit a2bd72045781578e64b51bbf145ff6690e26e3b4.
    
    ---------
    
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/bi/constant/ApiPaths.java
+++ backend/src/main/java/com/bi/constant/ApiPaths.java
@@ -1,8 +1,8 @@
 package com.bi.constant;
 
 public class ApiPaths {
-    public static final String HEALTHCHECK = "/healthcheck";
     private static final String BASE_API = "/api/v1";
+    public static final String HEALTHCHECK = BASE_API + "/healthcheck";
     public static final String GET_COFFEE_LISTINGS = BASE_API + "/coffee-listings";
     public static final String GET_RECIPES = BASE_API + "/recipes";
     public static final String LOGIN_WITH_X = BASE_API + "/login";
