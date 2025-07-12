23533e1267d7943f5bda27e981e447e8fae73fad
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-07-12 14:14:17 +0800

    fix: update script and header padding
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/scripts/restart-app.sh
+++ backend/scripts/restart-app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source /home/ec2-user/myapp/.env.prod
+
 pkill -f 'java -jar /home/ec2-user/myapp/myapp.jar'
 nohup java -jar /home/ec2-user/myapp/myapp.jar > /home/ec2-user/myapp/app.log 2>&1 &
 echo "App restarted"
