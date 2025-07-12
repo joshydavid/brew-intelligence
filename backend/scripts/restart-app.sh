7936895c507319fbe68ea509020f591246a94615
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-07-12 14:31:19 +0800

    fix: cd script
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/scripts/restart-app.sh
+++ backend/scripts/restart-app.sh
@@ -2,5 +2,5 @@
 source /home/ec2-user/myapp/.env.prod
 
 pkill -f 'java -jar /home/ec2-user/myapp/brewintelligence-backend-0.0.1-SNAPSHOT.jar'
-nohup java -jar /home/ec2-user/myapp/brewintelligence-backend-0.0.1-SNAPSHOT.jar > /home/ec2-user/myapp/brewintelligence-backend-0.0.1-SNAPSHOT.jar.log 2>&1 &
+nohup java -jar /home/ec2-user/myapp/brewintelligence-backend-0.0.1-SNAPSHOT.jar > /home/ec2-user/myapp/brewintelligence-backend-0.0.1-SNAPSHOT.log 2>&1 &
 echo "App restarted"
