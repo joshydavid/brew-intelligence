d7ae961f0007fc397dc3d5313d48fab1fb736087
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-07-12 15:14:59 +0800

    fix: remove api versioning for login endpoint
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/bi/constant/ApiPaths.java
+++ backend/src/main/java/com/bi/constant/ApiPaths.java
@@ -5,7 +5,7 @@ public class ApiPaths {
     private static final String BASE_API = "/api/v1";
     public static final String GET_COFFEE_LISTINGS = BASE_API + "/coffee-listings";
     public static final String GET_RECIPES = BASE_API + "/recipes";
-    public static final String LOGIN_WITH_X = BASE_API + "/login";
+    public static final String LOGIN_WITH_X = "/login";
     public static final String X_CALLBACK = "/callback";
     public static final String X_AUTH_STATUS = "/status";
     public static final String X_USER = BASE_API + "/user";
