a2444d6330e402f97bd520d0b7fcddb245c8f494
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-08-01 09:52:08 +0800

    feat: add banner component
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- frontend/app/components/Header/index.tsx
+++ frontend/app/components/Header/index.tsx
@@ -3,20 +3,28 @@
 import LoginX from "@/components/Authentication/LoginX";
 import BackButton from "@/components/BackButton";
 import { AppearanceToggle } from "@/components/ui/appearance-toggle";
+
 import { useAuthStatus } from "@/hooks/use-auth-status";
 import { usePathname } from "next/navigation";
+import BannerComponent from "../Banner";
 
 export default function Header() {
   const pathname = usePathname();
   const { authData } = useAuthStatus();
 
   return (
-    <header className="flex flex-row items-center justify-between px-4 py-6 md:px-6">
-      <div>{pathname !== "/" && <BackButton />}</div>
-      <div className="flex items-center gap-2">
-        <LoginX name={authData?.name ?? ""} />
-        <AppearanceToggle />
-      </div>
-    </header>
+    <>
+      <BannerComponent
+        title="⚠️&nbsp; Notice: AWS resources have been spun down. The backend is
+      currently not running."
+      />
+      <header className="flex flex-row items-center justify-between px-4 py-6 md:px-6">
+        <div>{pathname !== "/" && <BackButton />}</div>
+        <div className="flex items-center gap-2">
+          <LoginX name={authData?.name ?? ""} />
+          <AppearanceToggle />
+        </div>
+      </header>
+    </>
   );
 }
